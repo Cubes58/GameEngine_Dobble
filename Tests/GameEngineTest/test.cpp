3a645f2840d7a100a3ca60eaeb8f189835618871
Cubes58 <connorlewisrowland58@gmail.com>
2018-12-12 23:54:29 +0000

    Project now builds in for the 64 bit platform.

--- Tests/GameEngineTest/test.cpp
+++ Tests/GameEngineTest/test.cpp
@@ -1,14 +1,6 @@
 #include "pch.h"
 
-#define private public
-#include "Game.h"
-
 TEST(TestCaseName, TestName) {
-  //EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
-}
-
-int main(int ac, char *av[]) {
-	testing::InitGoogleTest(&ac, av);
-	return RUN_ALL_TESTS();
 }
